87e6d8d94056f6cf5913a6e6cb8a5204ce5d345b
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-07-24 11:45:42 +0200

    move scripts to directory

--- scripts/chameleons/chameleon_slouch_allometry.R
+++ scripts/chameleons/chameleon_slouch_allometry.R
@@ -13,7 +13,8 @@ library(ggphylomorpho)
 DataCham <- read_csv("data/chameleons/largest_chameleons.csv")
 
 
-ChamTree<-read.nexus("data/Tolley2013trimmedL.trees")
+ChamTree<-read.nexus("data/chameleons/Tolley2013trimmedL.trees")
+#ChamTree2<-read.nexus("data/Tolley2013trimmed.trees")
 #plotTree(ChamTree,ftype="i",fsize=0.6,lwd=1) #To gaze upon its beauty
 
 ## Reorder dataframe so the species labels match the tree tip labels
@@ -28,13 +29,13 @@ tree_height <- max(node.depth.edgelength(ChamTree))
 
 m0 <- slouch.fit(phy = ChamTree,
                  hillclimb = T,
-                 #vy_values = seq(0.001, 0.15, length.out = 30),
-                 #hl_values = seq(0.1, 150, length.out = 30),
+                 vy_values = seq(0.0001, 0.05, length.out = 30),
+                 hl_values = seq(0.001, 100, length.out = 30),
                  species = DataCham$Species,
                  response = DataCham$logpter,
                  direct.cov = DataCham$logbsl,
-                 mv.response = rep(0.1 * var(DataCham$logpter), length(ChamTree$tip.label)), #plus 5.5% intraspecific variation
-                 mv.direct.cov = rep(0.1 * var(DataCham$logbsl), length(ChamTree$tip.label))) #plus 5.5% intraspecific variation
+                 mv.response = rep(0.02 * var(DataCham$logpter), length(ChamTree$tip.label)), #plus 5.5% intraspecific variation
+                 mv.direct.cov = rep(0.02 * var(DataCham$logbsl), length(ChamTree$tip.label))) #plus 5.5% intraspecific variation
 plot(m0)
 summary(m0)
 
@@ -52,17 +53,13 @@ for (i in 1:length(ChamTree$tip.label)){
 ape::write.nexus.data(l, file = "data/chameleons/logpter_logbsl_residuals.nex", format = "continuous")
 
 ## Save the regimes to a nexusfile
-
-hypotheses <- c("Regime")
-for (hyp in hypotheses){
-  l2 <- list()
-  for (i in 1:length(ttreeA$tip.label)){
-    splab <- ChamTree$tip.label[i]
-    
-    dat <- DataCham[DataCham$Species == splab,hyp]
-    dat1 <- unlist(dat)
-    dat2 <- unname(dat1)
-    l2[splab] <- list(dat2)
-  }
-  ape::write.nexus.data(l2, file = paste0("data/chameleons/regimes_", hyp, ".nex"), format = "standard")
+l2 <- list()
+for (i in 1:length(ChamTree$tip.label)){
+  splab <- ChamTree$tip.label[i]
+  
+  dat <- DataCham[DataCham$Species == splab,hyp]
+  dat1 <- unlist(dat)
+  dat2 <- unname(dat1)
+  l2[splab] <- list(dat2)
 }
+ape::write.nexus.data(l2, file = paste0("data/chameleons/regimes.nex"), format = "standard")
